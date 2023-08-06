d423cac72fbfccc84f34343c5876c190d723adda
Etienne Trzpit <etienne.trzpit@free.fr>
2023-08-06 11:58:24 +0200

    modal start

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -5,7 +5,26 @@ const all = document.querySelector(".all");
 const object = document.querySelector(".object");
 const tenement = document.querySelector(".tenement");
 const hotel = document.querySelector(".hotel");
+const modal = document.querySelector(".modal");
 let category1 = [];
+localStorage.getItem("token");
+
+//vérifier si l'utilisateur est connecté
+if (localStorage.getItem("token")) {
+  //si oui, afficher logout au lieu de login
+  let login = document.querySelector(".login");
+  login.textContent = "Logout";
+  login.href = "index.html";
+  //ajout d'un event listener sur le bouton logout
+  login.addEventListener("click", () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  });
+} else {
+  //si non, cacher div edit
+  let edit = document.querySelector(".edit");
+  edit.style.display = "none";
+}
 
 async function fetchWorks() {
   const response = await fetch("http://localhost:5678/api/works");
@@ -119,3 +138,45 @@ object.addEventListener("click", displayCategory1);
 tenement.addEventListener("click", displayCategory2);
 hotel.addEventListener("click", displayCategory3);
 all.addEventListener("click", displayAll);
+
+async function fetchWorksModal() {
+  let photos = document.querySelector(".photos");
+  const response = await fetch("http://localhost:5678/api/works");
+  const json = await response.json();
+  Object.entries(json).forEach(([key, value]) => {
+    let figure = document.createElement("figure");
+    let img = document.createElement("img");
+    let figcaption = document.createElement("figcaption");
+    img.src = value.imageUrl;
+    img.alt = value.title;
+    figcaption.textContent = "éditer";
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    photos.appendChild(figure);
+  });
+}
+
+fetchWorksModal();
+
+//ajout d'un event listener sur mode édition
+document.querySelector(".edit p").addEventListener("click", () => {
+  //afficher modal
+  modal.showModal();
+});
+
+modal.addEventListener("click", (e) => {
+  const modalDimensions = modal.getBoundingClientRect();
+  if (
+    e.clientX < modalDimensions.left ||
+    e.clientX > modalDimensions.right ||
+    e.clientY < modalDimensions.top ||
+    e.clientY > modalDimensions.bottom
+  ) {
+    modal.close();
+  }
+});
+
+//ajout d'un event listener sur la croix de la modal
+document.querySelector(".close").addEventListener("click", () => {
+  modal.close();
+});
